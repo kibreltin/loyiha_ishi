443b9bbd0d3a6ac455513816251f4edd27c61f5c
kibreltin <eltinqoqindiq09@gmail.com>
2024-12-13 15:16:29 +0500

    Transport yo'l hodisalar bazasi

---
+++ yo__l_trasport.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Дек 13 2024 г., 10:14
+-- Версия сервера: 10.4.6-MariaDB
+-- Версия PHP: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `Yoʻl harakat havfsizlik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `yoʻl trasport`
+--
+
+CREATE TABLE `yoʻl trasport` (
+  `Id` int(11) DEFAULT NULL,
+  `Hodisa` varchar(15) NOT NULL,
+  `Tranport` varchar(15) NOT NULL,
+  `Jarima` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
